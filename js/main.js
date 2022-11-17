88fc1f68290245f06060bf59f1f74a337a5607bf
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-11-17 00:04:50 -0300

    Uploading: Loading messages from Json and traversing the Array

--- js/main.js
+++ js/main.js
@@ -1,8 +1,100 @@
-$(document).ready(function(){
-    $('.bxslider').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200,
-        responsive: true
-      });
-})
+$(document).ready(function () {
+  $(".bxslider").bxSlider({
+    mode: "fade",
+    captions: true,
+    slideWidth: 1200,
+    responsive: true,
+    pager: true,
+  });
+
+  // posts
+  var posts = [
+    {
+      title: "Titulo del Articulo 1",
+      Date:
+        "Plublicado el dia: " +
+        moment().date() +
+        " de " +
+        moment().format("MMMM") +
+        " del " +
+        moment().format("YYYY"),
+      content:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci corrupti temporibus iure reiciendis nemo velit vel nesciunt possimus rem perspiciatis qui aspernatur rerum odit dolorem saepe quibusdam ipsa prae sentium amet earum tenetur impedit. Quos eius amet minima soluta animi  iste!",
+    },
+    {
+      title: "Titulo del Articulo 2",
+      Date:
+        "Plublicado el dia: " +
+        moment().date() +
+        " de " +
+        moment().format("MMMM") +
+        " del " +
+        moment().format("YYYY"),
+      content:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci corrupti temporibus iure reiciendis nemo velit vel nesciunt possimus rem perspiciatis qui aspernatur rerum odit dolorem saepe quibusdam ipsa prae sentium amet earum tenetur impedit. Quos eius amet minima soluta animi  iste!",
+    },
+    {
+      title: "Titulo del Articulo 3",
+      Date:
+        "Plublicado el dia: " +
+        moment().date() +
+        " de " +
+        moment().format("MMMM") +
+        " del " +
+        moment().format("YYYY"),
+      content:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci corrupti temporibus iure reiciendis nemo velit vel nesciunt possimus rem perspiciatis qui aspernatur rerum odit dolorem saepe quibusdam ipsa prae sentium amet earum tenetur impedit. Quos eius amet minima soluta animi  iste!",
+    },
+    {
+      title: "Titulo del Articulo 4",
+      Date:
+        "Plublicado el dia: " +
+        moment().date() +
+        " de " +
+        moment().format("MMMM") +
+        " del " +
+        moment().format("YYYY"),
+      content:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci corrupti temporibus iure reiciendis nemo velit vel nesciunt possimus rem perspiciatis qui aspernatur rerum odit dolorem saepe quibusdam ipsa prae sentium amet earum tenetur impedit. Quos eius amet minima soluta animi  iste!",
+    },
+    {
+      title: "Titulo del Articulo 5",
+      Date:
+        "Plublicado el dia: " +
+        moment().date() +
+        " de " +
+        moment().format("MMMM") +
+        " del " +
+        moment().format("YYYY"),
+      content:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci corrupti temporibus iure reiciendis nemo velit vel nesciunt possimus rem perspiciatis qui aspernatur rerum odit dolorem saepe quibusdam ipsa prae sentium amet earum tenetur impedit. Quos eius amet minima soluta animi  iste!",
+    },
+    {
+        title: "Titulo del Articulo 6",
+        Date:
+          "Plublicado el dia: " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " del " +
+          moment().format("YYYY"),
+        content:
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci corrupti temporibus iure reiciendis nemo velit vel nesciunt possimus rem perspiciatis qui aspernatur rerum odit dolorem saepe quibusdam ipsa prae sentium amet earum tenetur impedit. Quos eius amet minima soluta animi  iste!",
+      },
+  ];
+  posts.forEach((item, index) => {
+    var post = ` 
+    <article class="post">
+      <h2>${item.title}</h2>
+      <span class="date">${item.date}</span>
+      <p>
+        ${item.content}
+      </p>
+      <a href="#" class="button-more">
+        Leer mas
+      </a>
+    </article>; 
+    `;
+    $("#posts").append(post);
+  });
+});
